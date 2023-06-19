ccfa9ed3bb156138efd94e3345dc4017250735cf
MohammedEitah <engeitah@gmail.com>
2023-06-19 23:34:23 +0300

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-int main (void)
-{
 /*
  * main is the entry function to any c program
  * main (void) means the function takes no argument and returns an integre
  */
-	puts ("Programming is like building a multilingual puzzle\n");
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
