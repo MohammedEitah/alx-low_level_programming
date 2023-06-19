666541917e86dcda441682bed1a7078d16db7088
MohammedEitah <engeitah@gmail.com>
2023-06-19 23:40:23 +0300

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-/*
- * main is the entry function to any c program
- * Description : main (void) means the function takes no argument and returns an integre
- */
+/**
+ * main is the entry point to any c program
+ *
+ * Description : main (void) means the function takes no argument 
+ * and returns an integre
+ *
+ *Return: 0 (Success)
+ * /
 
 int main(void)
 {
