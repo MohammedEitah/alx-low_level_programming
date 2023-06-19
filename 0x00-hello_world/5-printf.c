079b07da94d6164f4662c44e19d461d039a31618
MohammedEitah <engeitah@gmail.com>
2023-06-19 23:52:49 +0300

    task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /*
  * main - entry function
- * description : main is a very helpful function
+ * description : main(void) takes no argument
+ * returns an integer
  * return : 0 ok
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 	return (0);
 }
