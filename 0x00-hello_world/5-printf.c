33496d0e98715790612abe19b8a091d87e156873
MohammedEitah <engeitah@gmail.com>
2023-06-19 23:49:10 +0300

    task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - entry function
+ * description : main is a very helpful function
+ * return : 0 ok
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
