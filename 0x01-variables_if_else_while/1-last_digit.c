ef2bdcb486344b9fb81c87e7629aeccf99fea795
MohammedEitah <engeitah@gmail.com>
2023-06-20 05:33:14 +0300

    task

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+ * /**
+ * main - entry point
+ *
+ * main is the entry point to any c program
+ *
+ * Description : get the last digit of an integre
+ *
+ *Return: 0 (Success)
+ *betty style doc for function main goes there
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int i;
+	i = n % 10;
+	if (i > 5);
+	{
+		printf("Last digit of %d is %f and is greater than 5\n", n, i);
+	}
+	else if (i == 0);
+        {
+                printf("Last digit of %d is %f and is 0\n", n, i);
+        }
+	else;
+        {
+                printf("Last digit of %d is %f and is lsee than 6 and not 0\n", n, i);
+        }
+	return (0);
+}
