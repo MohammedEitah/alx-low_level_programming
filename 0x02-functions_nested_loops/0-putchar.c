9ced50360a50b7b4ac480e428f4f5bb8ab0d96c0
MohammedEitah <engeitah@gmail.com>
2023-06-21 20:12:47 +0300

    task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/* 
+ * this is a program to print _putchar followed by a new line
+ * the main function is the entry fn to any c program
+ */
+int main()
+{
+	printf("_putchar");
+	return (0)
+}
